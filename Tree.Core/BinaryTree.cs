2f03b7c5f89c3720cd5dda03c08d8740d23787f8
menalb <menalb84@gmail.com>
2017-03-19 20:50:37 +0000

    added projects for core and test

---
+++ Tree.Core/BinaryTree.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace Tree.Core
+{
+    public class Node
+    {
+        public int key;
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        public static Node CreateNewNode(int k)
+        {
+            return new Node { key = k };
+        }
+    }
+
+    public class BinaryTree
+    {
+        private readonly Node _root;
+        public BinaryTree(Node root)
+        {
+            _root = root;
+        }
+
+        private static bool findPath(Node root, IList<int> path, int k)
+        {
+            if (root == null) return false;
+
+            path.Add(root.key);
+
+            if (root.key == k) return true;
+
+            if (root.Left != null && findPath(root.Left, path, k) ||
+                (root.Right != null && findPath(root.Right, path, k)))
+                return true;
+
+            path.RemoveAt(path.Count - 1);
+            return false;
+        }
+
+        public int? findLCA(int n1, int n2)
+        {
+            var path1 = new List<int>();
+            var path2 = new List<int>();
+
+            if (!findPath(_root, path1, n1) || !findPath(_root, path2, n2))
+                return null;
+            
+            for (int i = 0; i < path1.Count && i < path2.Count; i++)
+            {
+                if (path1[i] != path2[i])
+                    return path1[i - 1];
+            }
+            return _root.key;
+        }
+    }
+}
---
+++ Tree.Tests/BinaryTreeTests.cs
@@ -0,0 +1,92 @@
+﻿using Tree.Core;
+using Xunit;
+
+namespace Tree.Tests
+{
+    public class LowestCommonAncestorTests
+    {
+        private readonly Node _root;
+        private readonly BinaryTree _sut;
+
+        public LowestCommonAncestorTests()
+        {
+            _root = GetTreeRootNode();
+            _sut = new BinaryTree(_root);
+        }
+
+        [Fact]
+        public void GivenABinaryTree_WhenTheTwoNodesAreOnTheRightSideOfTheTree_ItReturnsTheLCA()
+        {            
+            var lca = _sut.findLCA(6, 7);
+
+            Assert.Equal(3, lca);
+        }
+
+        [Fact]
+        public void GivenABinaryTree_WhenTheTwoNodesAreOnTheLeftSideOfTheTree_ItReturnsTheLCA()
+        {            
+            var lca = _sut.findLCA(4, 5);
+
+            Assert.Equal(2, lca);
+        }
+
+        [Fact]
+        public void GivenABinaryTree_WhenTheTwoNodesHaveDifferentDepthInTheTree_ItReturnsTheLCA()
+        {            
+            var lca = _sut.findLCA(2, 87);
+
+            Assert.Equal(1, lca);
+        }
+
+        [Fact]
+        public void GivenABinaryTree_WhenOneOfTHeNodesIaTheRootOfTheTree_ItReturnsTheRoot()
+        {            
+            var lca = _sut.findLCA(1, 87);
+
+            Assert.Equal(_root.key, lca);
+        }
+
+        [Fact]
+        public void GivenABinaryTree_WhenTheRootIsTheLCA_ItReturnsTheRoot()
+        {            
+            var lca = _sut.findLCA(4, 7);
+
+            Assert.Equal(_root.key, lca);
+        }
+
+        [Fact]
+        public void GivenABinaryTree_WhenOneOfTHeTwoNodesIsNotInTheTree_ItReturnsNoValue()
+        {            
+            var lca = _sut.findLCA(10, 7);
+
+            Assert.False(lca.HasValue);
+        }
+
+        /// <summary>
+        /// build binarytree
+        ///       1
+        ///  2        3
+        /// 4 5    6     7
+        ///       11 34 67 87
+        /// </summary>
+        /// <returns></returns>
+        private static Node GetTreeRootNode()
+        {
+            Node root = Node.CreateNewNode(1);
+            Node left = Node.CreateNewNode(2);
+            left.Left = Node.CreateNewNode(4);
+            left.Right = Node.CreateNewNode(5);
+            root.Left = left;
+            Node right = Node.CreateNewNode(3);
+            right.Left = Node.CreateNewNode(6);
+            right.Right = Node.CreateNewNode(7);
+            right.Left.Left= Node.CreateNewNode(11);
+            right.Left.Right= Node.CreateNewNode(34);
+            right.Right.Left= Node.CreateNewNode(67);
+            right.Right.Right= Node.CreateNewNode(87);
+            root.Right = right;
+
+            return root;
+        }
+    }
+}
